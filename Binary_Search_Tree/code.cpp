8b3136d98928a196c7888cb485bad1507c64a3c9
Prajakta Mane <prajakta916mane1@gmail.com>
2020-12-23 12:17:20 +0530

    Update code.cpp

--- Binary_Search_Tree/code.cpp
+++ Binary_Search_Tree/code.cpp
@@ -1,16 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
-
-//finding minmum element
-
+//finding minimum element
 int minValue(Node* root)
 {  
     if(root==NULL)
       return -1;
     return (!root->left)?root->data:minValue(root->left);
 }
+
+//finding sum of leaf nodes
+int sumOfLeafNodes(Node *r ){
+     /*Your code here */
+      if(r==NULL)
+       return 0;
+      if((r->left==NULL && r->right==NULL))
+       return r->data;
+       return sumOfLeafNodes(r->left)+sumOfLeafNodes(r->right);   
+}
+
+//search an element in bst
+bool search(Node* root, int x)
+{
+    if(root==NULL)
+    return false;
+    else{
+        if(root->data==x)
+         return true;
+    else{
+        if(root->data<x)
+        search(root->right,x);
+        else if(root->data>x)
+         search(root->left,x);
+        else
+        return false;
+       }
+    }
+}
+
 
 int main()
 {
