246ac31cd5461367f9e1370d23acaa8f5561937e
Prajakta Mane <prajakta916mane1@gmail.com>
2020-12-27 11:42:40 +0530

    Create Top_View_bt.cpp

---
+++ Binary_Tree/Top_View_bt.cpp
@@ -0,0 +1,27 @@
+void map_create(Node*root,int dist,int level,map<int,pair<int,int>>&mp)
+{
+    if(root)
+    {
+        if(mp.find(dist)==mp.end() || mp[dist].second>level)
+        {
+            mp[dist]={root->data,level};
+        }
+        map_create(root->left,dist-1,level+1,mp);
+        map_create(root->right,dist+1,level+1,mp);
+    }
+}
+void topView(struct Node *root)
+{
+    //Your
+    
+    map<int,pair<int,int>>mp;
+    if(root)
+    {
+        map_create(root,0,0,mp);
+        for(auto it:mp)
+        {
+            cout<<it.second.first<<" ";
+        }
+       // cout<<endl;
+    }
+}
