f0cb8251ee0d3efd147a268340984904cd8d47eb
Prajakta Mane <prajakta916mane1@gmail.com>
2021-01-17 16:52:49 +0530

    Create LcsRecurssive.cpp

---
+++ DynamicProgramming/LongestCommonSubsequence/LcsRecurssive.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int lcs(string x ,string y ,int m,int n)
+{
+    if(m==0 || n==0)
+    return 0;
+    
+    if(x[n-1]==y[n-1])
+    return 1+lcs(x,y,m-1,n-1);
+    else
+    return max(lcs(x,y,m-1,n),lcs(x,y,m,n-1));
+}
+
+int main() {
+	// your code goes here
+	string x,y;
+	cin>>x>>y;
+	int m = x.length();
+	int n = y.length();
+	cout<<lcs(x,y,m,n);
+	return 0;
+}
