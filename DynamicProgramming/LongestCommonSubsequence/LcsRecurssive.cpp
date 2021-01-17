74c14ca4bab010fab696008845ec21e4c588c885
Prajakta Mane <prajakta916mane1@gmail.com>
2021-01-17 16:55:58 +0530

    Update LcsRecurssive.cpp

--- DynamicProgramming/LongestCommonSubsequence/LcsRecurssive.cpp
+++ DynamicProgramming/LongestCommonSubsequence/LcsRecurssive.cpp
@@ -6,7 +6,7 @@ int lcs(string x ,string y ,int m,int n)
     if(m==0 || n==0)
     return 0;
     
-    if(x[n-1]==y[n-1])
+    if(x[m-1]==y[n-1])
     return 1+lcs(x,y,m-1,n-1);
     else
     return max(lcs(x,y,m-1,n),lcs(x,y,m,n-1));
