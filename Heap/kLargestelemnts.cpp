0b710762362ce6f53c7e2e4ac91bb5478f73f721
Prajakta Mane <prajakta916mane1@gmail.com>
2021-02-01 22:35:18 +0530

    Update kLargestelemnts.cpp

--- Heap/kLargestelemnts.cpp
+++ Heap/kLargestelemnts.cpp
@@ -1,3 +1,18 @@
+/*
+link:https://practice.geeksforgeeks.org/problems/k-largest-elements4206/1
+
+Given an array Arr of N positive integers, find K largest elements from the array.  The output elements should be printed in decreasing order.
+
+Example 1:
+
+Input:
+N = 5, K = 2
+Arr[] = {12, 5, 787, 1, 23}
+Output: 787 23
+
+*/
+
+
 vector<int> kLargest(int arr[], int n, int k)
 {
     // code here
