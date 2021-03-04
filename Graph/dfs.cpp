db040b5e00d8f2916ed306bbf3cded3b4f95a584
Prajakta Mane <prajakta916mane1@gmail.com>
2021-03-04 19:35:38 +0530

    Update dfs.cpp

--- Graph/dfs.cpp
+++ Graph/dfs.cpp
@@ -25,31 +25,27 @@ Given a connected undirected graph. Perform a Depth First Traversal of the graph
 
 /* iterative */
 
-vector<int>dfs(int V, vector<int> adj[]){
-	    
-  stack<int>s;
-  vector<int>ans;
-  vector<bool>visited(V,false);
-  
-  s.push(0);
-  int flag=0;
-  ans.push_back(s.top());
-  while(!s.empty())
-  {
-     flag=0;
-     for(auto x:adj[s.top()])
-     {
-        if(visited[x]==false)
-        {
-           s.push(x);
-           ans.push_back(x);
-           flag=1;
-           visited[x]=true;
-           break;
+vector<int>dfsOfGraph(int V, vector<int> adj[]){
+	    // Code here
+    bool vis[V]={false};
+    vector<int>ans;
+    stack<int> st;
+    st.push(0);
+    ans.push_back(0);
+    vis[0]=1;
+    while(!st.empty()){
+        int top=st.top();
+        int flag=0;
+        for(auto x:adj[top]){
+            if(vis[x]!=1){
+                st.push(x);
+                ans.push_back(x);
+                vis[x]=1;
+                flag=1;
+                break;
+            }
         }
-     }
-     if(flag==0)
-       s.pop();
+        if(!flag) st.pop();
   }
   return ans;
   }
