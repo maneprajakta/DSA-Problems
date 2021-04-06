1bd0f94ced413af51c1f88629f13aed28daeffea
Prajakta Mane <prajakta916mane1@gmail.com>
2021-04-06 11:19:33 +0530

    Update Reverse_LL.cpp

--- Linked_List/Reverse_LL.cpp
+++ Linked_List/Reverse_LL.cpp
@@ -49,28 +49,7 @@ struct Node* reverseList(struct Node *head)
     return rest;
 }
 
-//reverse linled list in group of size
 
-struct node *reverse (struct node *head, int k)
-{ 
-    // Complete this method
-    node* curr=head;
-    node* next=NULL;
-    node* prev=NULL;
-    int count=0;
-    while(curr!=NULL && count<k)
-    {
-        next = curr->next;
-        curr->next=prev;
-        prev=curr;
-        curr=next;
-        count++;
-    }
-    if(next!=NULL) 
-    head->next = reverse(next,k);
-    
-    return prev;
-}
 
 
 //detect loop
