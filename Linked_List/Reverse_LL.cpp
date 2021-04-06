62a411b44176c773ecb0173c24629754991b054f
Prajakta Mane <prajakta916mane1@gmail.com>
2021-04-06 11:05:27 +0530

    Update Reverse_LL.cpp

--- Linked_List/Reverse_LL.cpp
+++ Linked_List/Reverse_LL.cpp
@@ -1,3 +1,19 @@
+/* 
+
+link :https://practice.geeksforgeeks.org/problems/reverse-a-linked-list/1
+
+Given a linked list of N nodes. The task is to reverse this list.
+
+Example 1:
+
+Input:
+LinkedList: 1->2->3->4->5->6
+Output: 6 5 4 3 2 1
+Explanation: After reversing the list, 
+elements are 6->5->4->3->2->1.
+
+*/
+
 struct Node* reverseList(struct Node *head)
 {
     // code here
