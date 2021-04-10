cbc66f51f3f9ebac509f931f64a2ce3c0ccefbb2
Prajakta Mane <prajakta916mane1@gmail.com>
2021-04-10 18:33:50 +0530

    Update spiral.cpp

--- Matrix/spiral.cpp
+++ Matrix/spiral.cpp
@@ -1,3 +1,24 @@
+/*
+link:https://practice.geeksforgeeks.org/problems/spirally-traversing-a-matrix-1587115621/1
+
+
+Spirally traversing a matrix 
+Medium Accuracy: 48.39% Submissions: 27716 Points: 4
+Given a matrix of size R*C. Traverse the matrix in spiral form.
+
+Example 1:
+
+Input:
+R = 4, C = 4
+matrix[][] = {{1, 2, 3, 4},
+           {5, 6, 7, 8},
+           {9, 10, 11, 12},
+           {13, 14, 15,16}}
+Output: 
+1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
+
+*/
+
  vector<int> spirallyTraverse(vector<vector<int> > matrix, int r, int c) 
     {
         // code here 
