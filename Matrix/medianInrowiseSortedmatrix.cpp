6c9ee3baae7e5298eabf502f1d886febce2d0e75
Prajakta Mane <prajakta916mane1@gmail.com>
2021-04-10 21:37:58 +0530

    Update medianInrowiseSortedmatrix.cpp

--- Matrix/medianInrowiseSortedmatrix.cpp
+++ Matrix/medianInrowiseSortedmatrix.cpp
@@ -1,3 +1,20 @@
+/*
+link : https://practice.geeksforgeeks.org/problems/median-in-a-row-wise-sorted-matrix1527/1
+
+Given a row wise sorted matrix of size RxC where R and C are always odd, find the median of the matrix.
+
+Example 1:
+
+Input:
+R = 3, C = 3
+M = [[1, 3, 5], 
+     [2, 6, 9], 
+     [3, 6, 9]]
+
+Output: 5
+
+*/
+
 int median(vector<vector<int>> &matrix, int r, int c){
         // code here 
         
