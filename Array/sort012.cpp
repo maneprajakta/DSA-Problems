7800f264dde4af3bca89daf1a0b56014e8f58033
Prajakta Mane <prajakta916mane1@gmail.com>
2021-06-20 12:57:11 +0530

    Update sort012.cpp

--- Array/sort012.cpp
+++ Array/sort012.cpp
@@ -1,3 +1,20 @@
+/*
+link: https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1#
+
+Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
+
+
+Example 1:
+
+Input: 
+N = 5
+arr[]= {0 2 1 2 0}
+Output:
+0 0 1 2 2
+Explanation:
+0s 1s and 2s are segregated 
+into ascending order.
+*/
 void sort012(int a[], int n)
 {
 int low = 0, mid = 0, high = n-1;
