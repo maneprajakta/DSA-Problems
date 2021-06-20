cf5a1e6325a372e23c544ffb7a60476f25d6350f
Prajakta Mane <prajakta916mane1@gmail.com>
2021-06-20 12:59:31 +0530

    Update sort012.cpp

--- Array/sort012.cpp
+++ Array/sort012.cpp
@@ -2,7 +2,7 @@
 link: https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1#
 
 Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
-
+(Dutch National Flag Algorithm)
 
 Example 1:
 
