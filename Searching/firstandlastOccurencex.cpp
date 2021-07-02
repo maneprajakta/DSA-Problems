cc072c40ef40ba8ec2b36a0ca1fdaa82589f8bc2
Prajakta Mane <prajakta916mane1@gmail.com>
2021-07-02 09:56:46 +0530

    Update firstandlastOccurencex.cpp

--- Searching/firstandlastOccurencex.cpp
+++ Searching/firstandlastOccurencex.cpp
@@ -1,3 +1,17 @@
+/*
+First and last occurrences of x
+Given a sorted array arr containing n elements with possibly duplicate elements, 
+the task is to find indexes of first and last occurrences of an element x in the given array.
+
+Example 1:
+
+Input:
+n=9, x=5
+arr[] = { 1, 3, 5, 5, 5, 5, 67, 123, 125 }
+Output:  2 5
+Explanation: First occurrence of 5 is at index 2 and last
+             occurrence of 5 is at index 5.
+*/
 int bsfirst(vector<long long> &v, long long x)
 {
     int res=-1;
