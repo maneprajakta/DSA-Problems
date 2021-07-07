3e0f5cabc66f66f660a1fe1d5321d1c63bc83f57
Prajakta Mane <prajakta916mane1@gmail.com>
2021-07-07 21:58:00 +0530

    Update Reverse_leavel_order_Traversal.cpp

--- Binary_Tree/Reverse_leavel_order_Traversal.cpp
+++ Binary_Tree/Reverse_leavel_order_Traversal.cpp
@@ -1,22 +1,39 @@
-vector<int> reverseLevelOrder(Node *root)
+/*
+Link:https://practice.geeksforgeeks.org/problems/reverse-level-order-traversal/1#
+Reverse Level Order Traversal 
+Easy Accuracy: 47.34% Submissions: 51052 Points: 2
+Given a binary tree of size N, find its reverse level order traversal. ie- the traversal must begin from the last level.
+
+Example 1:
+
+Input :
+        1
+      /   \
+     3     2
+
+Output: 3 2 1
+Explanation:
+Traversing level 1 : 3 2
+Traversing level 0 : 1
+*/
+
+vector<int> reverseLevelOrder(Node *node)
 {
+    // code here
     queue<Node*>q;
-    stack<int>s;
     vector<int>v;
-    q.push(root);
-    while(!q.empty())
+    
+    if(node!=NULL)
     {
-        Node*temp = q.front();
-        q.pop();
-        s.push(temp->data);
-        
-        if(temp->right)q.push(temp->right);
-        if(temp->left)q.push(temp->left);
+        q.push(node);
+        while(!q.empty())
+        {  Node*temp=q.front();
+           q.pop();
+           v.push_back(temp->data);
+           if(temp->right!=NULL)q.push(temp->right);
+           if(temp->left!=NULL)q.push(temp->left);
+        }
     }
-    while(!s.empty())
-    {
-        v.push_back(s.top());
-        s.pop();
-    }
+    reverse(v.begin(),v.end());
     return v;
 }
