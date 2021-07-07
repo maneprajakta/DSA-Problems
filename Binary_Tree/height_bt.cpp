28c8f34771e95e49dc8c429fe62d4f20f21efbff
Prajakta Mane <prajakta916mane1@gmail.com>
2021-07-07 22:12:53 +0530

    Update height_bt.cpp

--- Binary_Tree/height_bt.cpp
+++ Binary_Tree/height_bt.cpp
@@ -1,3 +1,19 @@
+/*
+Link:https://practice.geeksforgeeks.org/problems/height-of-binary-tree/1#
+Height of Binary Tree 
+Medium Accuracy: 65.76% Submissions: 100k+ Points: 4
+Given a binary tree, find its height.
+
+
+Example 1:
+
+Input:
+     1
+    /  \
+   2    3
+Output: 2
+
+*/
 int height(struct Node* node){
         // code here
         if(!node)
