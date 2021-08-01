0aa7a43c11e90259fa7231052dd64c3c6a7d432b
Prajakta Mane <prajakta916mane1@gmail.com>
2021-08-01 16:20:01 +0530

    Create ValidPalindrome.cpp

---
+++ String/ValidPalindrome.cpp
@@ -0,0 +1,28 @@
+/*
+link:https://leetcode.com/problems/valid-palindrome/
+Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+
+*/
+
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+    for (int i = 0, j = s.size() - 1; i < j; i++, j--) { // Move 2 pointers from each end until they collide
+        while (isalnum(s[i]) == false && i < j) i++; // Increment left pointer if not alphanumeric
+        while (isalnum(s[j]) == false && i < j) j--; // Decrement right pointer if no alphanumeric
+        if (toupper(s[i]) != toupper(s[j])) return false; // Exit and return error if not match
+    }
+    
+    return true;
+}
+};
