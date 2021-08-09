db3aad4350bfe3994f5f768ec5f010daa75ad7b0
Prajakta Mane <prajakta916mane1@gmail.com>
2021-08-09 16:38:27 +0530

    Update BubbleSort.cpp

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -24,3 +24,28 @@ void bubbleSort(int arr[], int n)
             }
         }
     }
+
+class Solution
+{
+    public:
+    //Function to sort the array using bubble sort algorithm.
+    void bubbleSort(int arr[], int n)
+    {
+        // Your code here 
+        bool swapped;
+        
+        for(int i=0;i<n;i++)
+        {   swapped=false;
+            for(int j=0;j<n-i-1;j++)
+            {
+                if(arr[j]>arr[j+1]){
+                swap(&arr[j],&arr[j+1]);
+                swapped=true;
+                }
+            }
+            if(swap==false)
+            break;
+        }
+        
+    }
+};
